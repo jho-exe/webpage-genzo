c32705b3db2e24f8ef37e6fb99346a83b888ba26
Javier Hormazabal <javier.hormazabal.182@gmail.com>
2023-05-23 13:32:10 -0400

    First commit webpage for Genzo

---
+++ js/index.js
@@ -0,0 +1,92 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.navegador', {
+	duration: 2500,
+	origin: 'bottom',
+	distance: '-100px'
+});
+
+sr.reveal('.titular', {
+	duration: 2500,
+	origin: 'bottom',
+	distance: '-100px'
+});
+
+sr.reveal('.logo', {
+	duration: 2500,
+	origin: 'bottom',
+	distance: '-100px'
+});
+
+sr.reveal('.todo-servicios', {
+	duration: 3000,
+	origin: 'top',
+	distance: '-100px'
+});
+
+sr.reveal('.blog', {
+	 duration: 3000,
+	origin: 'left',
+	distance: '-200px'
+});
+
+sr.reveal('.nosotros', {
+	duration: 3000,
+	origin: 'bottom',
+	distance: '-100px'
+});
+
+sr.reveal('.future', {
+	 duration: 3000,
+	origin: 'left',
+	distance: '-200px'
+});
+
+sr.reveal('.content', {
+	duration: 2500,
+	origin: 'bottom',
+	distance: '-100px'
+});
+
+sr.reveal('.services1', {
+	duration: 2500,
+	origin: 'bottom',
+	distance: '-100px'
+});
+
+sr.reveal('.services2', {
+	 duration: 3000,
+	origin: 'left',
+	distance: '-200px'
+});
+
+sr.reveal('.services4', {
+	 duration: 3000,
+	origin: 'left',
+	distance: '-200px'
+});
+
+sr.reveal('.services6', {
+	 duration: 3000,
+	origin: 'left',
+	distance: '-200px'
+});
+
+sr.reveal('.services3', {
+	 duration: 3000,
+	origin: 'right',
+	distance: '-200px'
+});
+
+sr.reveal('.services5', {
+	 duration: 3000,
+	origin: 'right',
+	distance: '-200px'
+});
+
+sr.reveal('.logo1', {
+	 duration: 2500,
+	origin: 'bottom',
+	distance: '-100px'
+});
+
